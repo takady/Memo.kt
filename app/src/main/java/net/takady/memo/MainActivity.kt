890b90b379fe703d82167410192725a9157ca663
Yuichi Takada <takadyuichi@gmail.com>
2016-01-31 23:04:34 +0900

    Initial commit

---
+++ app/src/main/java/net/takady/memo/MainActivity.kt
@@ -0,0 +1,17 @@
+package net.takady.memo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        myButton.setOnClickListener {
+            myTextView.text = "test"
+        }
+    }
+}
